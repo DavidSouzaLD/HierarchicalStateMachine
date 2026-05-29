55b3c6058538d823ea4a444a87fbfc57807e616b
David Souza <100738882+DavidSouzaLD@users.noreply.github.com>
2026-05-29 09:22:18 -0300

    Add files via upload

---
+++ Interfaces/IState.cs
@@ -0,0 +1,13 @@
+﻿namespace Radiopeaks.HSM
+{
+    public interface IState<TContext>
+    {
+        void Enter();
+        void Exit();
+        void Update();
+        void FixedUpdate();
+        void LateUpdate();
+        
+        void Initialize(TContext context, StateMachine<TContext> machine, IState<TContext> superState);
+    }
+}
