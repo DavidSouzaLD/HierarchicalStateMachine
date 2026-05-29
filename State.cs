00ce050d7371d50ea930c21cbed2b75bc08bfd45
David Souza <100738882+DavidSouzaLD@users.noreply.github.com>
2026-05-29 09:19:28 -0300

    Add files via upload

---
+++ State.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace Radiopeaks.HSM
+{
+    public abstract class State<TContext> : IState<TContext>
+    {
+        protected TContext Context { get; private set; }
+        protected StateMachine<TContext> StateMachine { get; private set; }
+
+        protected State<TContext> SuperState { get; private set; }
+        public State<TContext> SubState { get; private set; }
+
+        public virtual void Enter() { }
+        public virtual void Exit() { }
+        public virtual void Update() { }
+        public virtual void FixedUpdate() { }
+        public virtual void LateUpdate() { }
+
+        public void Initialize(TContext context, StateMachine<TContext> machine, IState<TContext> superState)
+        {
+            Context = context;
+            StateMachine = machine;
+            SuperState = superState as State<TContext>;
+        }
+
+        protected void SetSubState<T>() where T : IState<TContext>, new()
+        {
+            ChangeSubState(StateMachine.GetOrCreateState<T>());
+        }
+
+        protected void SwitchState<T>() where T : IState<TContext>, new()
+        {
+            var newState = StateMachine.GetOrCreateState<T>();
+
+            if (SuperState != null)
+            {
+                SuperState.ChangeSubState(newState);
+            }
+            else
+            {
+                StateMachine.ChangeState(newState);
+            }
+        }
+
+        private void ChangeSubState(IState<TContext> subState)
+        {
+            if (subState == null) return;
+
+            SubState?.Exit();
+
+            SubState = subState as State<TContext>;
+
+            SubState?.Initialize(Context, StateMachine, this);
+            SubState?.Enter();
+
+            StateMachine.LoadHierarchy();
+        }
+    }
+}
---
+++ StateMachine.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Radiopeaks.HSM
+{
+    public class StateMachine<TContext>
+    {
+        private readonly TContext _context;
+        private readonly List<IState<TContext>> _stateHierarchy = new();
+        private readonly Dictionary<Type, IState<TContext>> _stateCache = new();
+        private bool _isTransitioning;
+
+        public IState<TContext> CurrentState { get; private set; }
+
+        public StateMachine(TContext context)
+        {
+            _context = context;
+        }
+
+        public void Start<T>() where T : IState<TContext>, new()
+        {
+            ChangeState(GetOrCreateState<T>());
+        }
+
+        public void Update()
+        {
+            _isTransitioning = false;
+            for (int i = 0; i < _stateHierarchy.Count; i++)
+            {
+                if (_isTransitioning) break;
+                _stateHierarchy[i]?.Update();
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            _isTransitioning = false;
+            for (int i = 0; i < _stateHierarchy.Count; i++)
+            {
+                if (_isTransitioning) break;
+                _stateHierarchy[i]?.FixedUpdate();
+            }
+        }
+
+        public void LateUpdate()
+        {
+            _isTransitioning = false;
+            for (int i = 0; i < _stateHierarchy.Count; i++)
+            {
+                if (_isTransitioning) break;
+                _stateHierarchy[i]?.LateUpdate();
+            }
+        }
+
+        public void ChangeState(IState<TContext> newState)
+        {
+            if (newState == null) return;
+
+            CurrentState?.Exit();
+            CurrentState = newState;
+            CurrentState.Initialize(_context, this, null);
+            CurrentState.Enter();
+
+            LoadHierarchy();
+        }
+
+        public void LoadHierarchy()
+        {
+            if (CurrentState == null) return;
+
+            _stateHierarchy.Clear();
+            var state = CurrentState;
+            
+            while (state != null)
+            {
+                _stateHierarchy.Add(state);
+                state = (state as State<TContext>)?.SubState;
+            }
+
+            _isTransitioning = true;
+        }
+
+        public IState<TContext> GetOrCreateState<T>() where T : IState<TContext>, new()
+        {
+            var type = typeof(T);
+            if (!_stateCache.TryGetValue(type, out var state))
+            {
+                state = new T();
+                _stateCache[type] = state;
+            }
+            return state;
+        }
+
+#if UNITY_EDITOR
+        public string HierarchyString()
+        {
+            var value = "";
+            var i = 0;
+            foreach (var t in _stateHierarchy)
+            {
+                var name = t.GetType().Name;
+                for (var x = 0; x < i; x++) value += "       ";
+                value += $" -> {name} \n";
+                i++;
+            }
+            return value;
+        }
+#endif
+    }
+}
